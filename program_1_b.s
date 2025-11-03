2e211bbafea86a785fc593d4b47ef2972b73cd15
Tugceulu <67679907+Tugceulu@users.noreply.github.com>
2025-11-03 16:45:58 +0100

    Create program_1_b.s

---
+++ program_1_b.s
@@ -0,0 +1,113 @@
+.section .data
+v1: .space 128
+v2: .space 128
+v3: .space 128
+v4: .space 128
+v5: .space 128
+v6: .space 128
+v7: .word 1
+v8: .space 4
+v9: .space 4
+.section .text
+.globl _start
+_start:
+la s0, v1
+la s1, v2
+la s2, v3
+la s3, v4
+la s4, v5
+la s5, v6
+la s7, v7
+la s8, v8
+la s9, v9
+li t0, 30
+slli t1, t0, 2
+add s0, s0, t1
+add s1, s1, t1
+add s2, s2, t1
+add s3, s3, t1
+add s4, s4, t1
+add s5, s5, t1
+loop:
+flw fa0, 0(s0)
+flw fa1, 0(s1)
+flw fa2, 0(s2)
+lw t2, 0(s7)
+li t3, 3
+rem t4, t0, t3
+beqz t4, MULT3_1
+fcvt.s.w fa3, t2
+fcvt.s.w ft6, t0
+fmul.s ft7, fa3, ft6
+fmul.s fa4, fa0, ft7
+fcvt.w.s t2, fa4
+sw t2, 0(s7)
+j AFTER_IF_1
+MULT3_1:
+sll t4, t2, t0
+fcvt.s.w ft8, t4
+fdiv.s fa4, fa0, ft8
+fcvt.w.s t2, fa4
+sw t2, 0(s7)
+AFTER_IF_1:
+fmul.s ft0, fa4, fa0
+fsub.s ft1, ft0, fa1
+fsw ft1, 0(s3)
+fdiv.s ft2, ft1, fa2
+flw fa5, 0(s9)
+fsub.s ft3, ft2, fa5
+fsw ft3, 0(s4)
+fsw ft3, 0(s9)
+fsub.s ft4, ft1, fa0
+fmul.s ft5, ft4, ft3
+fsw ft5, 0(s5)
+addi s0, s0, -4
+addi s1, s1, -4
+addi s2, s2, -4
+addi s3, s3, -4
+addi s4, s4, -4
+addi s5, s5, -4
+addi t0, t0, -1
+flw fa0, 0(s0)
+flw fa1, 0(s1)
+flw fa2, 0(s2)
+lw t2, 0(s7)
+li t3, 3
+rem t4, t0, t3
+beqz t4, MULT3_2
+fcvt.s.w fa3, t2
+fcvt.s.w ft6, t0
+fmul.s ft7, fa3, ft6
+fmul.s fa4, fa0, ft7
+fcvt.w.s t2, fa4
+sw t2, 0(s7)
+j AFTER_IF_2
+MULT3_2:
+sll t4, t2, t0
+fcvt.s.w ft8, t4
+fdiv.s fa4, fa0, ft8
+fcvt.w.s t2, fa4
+sw t2, 0(s7)
+AFTER_IF_2:
+fmul.s ft0, fa4, fa0
+fsub.s ft1, ft0, fa1
+fsw ft1, 0(s3)
+fdiv.s ft2, ft1, fa2
+flw fa5, 0(s9)
+fsub.s ft3, ft2, fa5
+fsw ft3, 0(s4)
+fsw ft3, 0(s9)
+fsub.s ft4, ft1, fa0
+fmul.s ft5, ft4, ft3
+fsw ft5, 0(s5)
+addi s0, s0, -4
+addi s1, s1, -4
+addi s2, s2, -4
+addi s3, s3, -4
+addi s4, s4, -4
+addi s5, s5, -4
+addi t0, t0, -1
+bge t0, x0, loop
+li a0, 0
+li a7, 93
+ecall
